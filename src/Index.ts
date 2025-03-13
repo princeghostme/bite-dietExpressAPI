51a871fee1985815c87aac538fbf3edfa7a7c8d6
princeghostme <coolkidzzz207@gmail.com>
2025-03-13 10:40:54 +0530

    fixes

--- src/Index.ts
+++ src/Index.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import routes from "./routes";
 import errorHandlingMiddlware from "./Utility/errorHandlingMiddlware";
 import mongoUsers from "./DbContext/mongoUsers";
+import { myAppRes } from "./Utility/appResponse";
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,9 @@ app.use(cors());
 app.use(express.json());
 mongoUsers();
 
+app.use("/",(req:Request, res:Response)=>{
+    myAppRes(res,{data:"Service Started"})
+});
 app.use("/api",routes);
 app.use(errorHandlingMiddlware);
 
